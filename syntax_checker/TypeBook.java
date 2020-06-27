284e7b66e95f1c6f240db916954cad66ab790435
dh001@ucr.edu <dh001@ucr.edu>
2020-06-27 16:44:16 -0700

    added TypeBook class with Int/arr/bool

---
+++ syntax_checker/TypeBook.java
@@ -0,0 +1,13 @@
+package syntax_checker;
+
+public class TypeBook extends Book {
+    public TypeBook(){}
+}
+
+class IntBook extends TypeBook{}
+class ArrayBook extends TypeBook{}
+class BoolBook extends TypeBook{}
+
+class ClassTypeBook extends TypeBook{
+    String classname;
+}
