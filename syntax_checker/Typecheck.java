0474e5a7fc8c4a03e1c4e24b5a9ccc3a4baf3107
dh001@ucr.edu <dh001@ucr.edu>
2020-07-06 17:24:21 -0700

    rough verision

---
+++ syntax_checker/Typecheck.java
@@ -0,0 +1,12 @@
+package syntax_checker;
+import syntaxtree.*;
+import parser.*;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Typecheck {
+
+    public static MiniJavaParser parser =  new MiniJavaParser(System.in);
+    public static SymbolTable symbolTable;
+
+}
